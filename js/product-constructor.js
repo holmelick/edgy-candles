9406c62e67afd90b184f32203a17f5bf61f3f10b
Holly Melick <holmelick@wl.k12.ia.us>
2018-10-16 14:30:07 -0500

    Added products and filters

---
+++ js/product-constructor.js
@@ -0,0 +1,33 @@
+"use strict";
+
+
+
+var product_list=[];
+var Product = function(product){
+    this.image = product.image;
+    this.title=product.title;
+    this.price=product.price;
+    this.size=product.size;
+    this.description=product.description;
+    this.colors=product.colors;
+    this.materials=product.materials;
+    this.brand=product.brand;
+    this.categories=product.categories;
+    this.product_id=product.product_id;
+};
+
+Product.prototype.toHtml = function(){
+    var source = $('#product-template').text();
+    var template = Handlebars.compile(source);
+    return template(this);
+};
+
+products.forEach(function(product){
+    product_list.push(new Product(product));
+
+});
+
+
+product_list.forEach(function(product){
+    $('#product-listings').append(product.toHtml());
+});
